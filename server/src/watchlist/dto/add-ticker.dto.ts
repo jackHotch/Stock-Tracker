edf2f2c80cdf0bde9a54021ee97d85bd2b5ad0ab
jackHotch <jhotchkiss2021@gmail.com>
2026-06-07 00:06:14 -0400

    updated dtos for watchlist

--- server/src/watchlist/dto/add-ticker.dto.ts
+++ server/src/watchlist/dto/add-ticker.dto.ts
@@ -1,1 +1,9 @@
-export class AddTickerDto {}
+import { IsString, IsNotEmpty, MaxLength, Matches } from 'class-validator';
+
+export class AddTickerDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(10)
+  @Matches(/^[A-Z]+$/, { message: 'ticker must be uppercase letters only' })
+  ticker!: string;
+}
--- server/src/watchlist/dto/watchlist-item.dto.ts
+++ server/src/watchlist/dto/watchlist-item.dto.ts
@@ -1,1 +1,15 @@
-export class WatchlistItem {}
+import { IsInt, IsPositive, IsString, IsNotEmpty, MaxLength, IsDate } from 'class-validator';
+
+export class WatchlistItem {
+  @IsInt()
+  @IsPositive()
+  id!: number;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(10)
+  ticker!: string;
+
+  @IsDate()
+  added_at!: Date;
+}
