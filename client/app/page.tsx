65179724ca21de79612b35958601c2dafd469c06
jackHotch <jhotchkiss2021@gmail.com>
2026-07-26 21:05:32 -0400

    finished login page ui

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,19 +1,35 @@
+import { Container } from "@/components/container"
 import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { TrendingUp } from "lucide-react"
 
-export default function Page() {
+export default function Login() {
   return (
-    <div className="flex min-h-svh p-6">
-      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
+    <div className="flex min-h-screen min-w-screen flex-col gap-12 p-6">
+      <h1 className="flex items-center gap-2">
+        MarketTrend <TrendingUp size={36} color="var(--primary)" />
+      </h1>
+      <Container className="flex w-full flex-col gap-8 p-6 md:mx-auto md:max-w-130">
         <div>
-          <h1 className="font-medium">Project ready!</h1>
-          <p>You may now add components and start building.</p>
-          <p>We&apos;ve already added the button component for you.</p>
-          <Button className="mt-2">Button</Button>
+          <h2>Welcome back</h2>
+          <p>Sign in to your account to continue</p>
         </div>
-        <div className="font-mono text-xs text-muted-foreground">
-          (Press <kbd>d</kbd> to toggle dark mode)
+
+        <div className="space-y-1">
+          <Label htmlFor="email">Email</Label>
+          <Input id="email" placeholder="example@gmail.com" />
         </div>
-      </div>
+
+        <div className="space-y-1">
+          <Label htmlFor="password">Password</Label>
+          <Input id="password" type="password" />
+        </div>
+
+        <Button size="lg" className="w-full">
+          Login
+        </Button>
+      </Container>
     </div>
   )
 }
---
+++ client/components/container.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from "react"
+import { cn } from "@/lib/utils"
+
+interface ContainerProps {
+  className?: string
+  children?: ReactNode | ReactNode[]
+}
+
+export const Container = ({ className, children }: ContainerProps) => {
+  return <div className={cn("corner-marks border border-border p-4", className)}>{children}</div>
+}
